508c408a3d5c233f245110fd48b30b20905c27ba
Nathan Dunn <ndunn@me.com>
2019-10-01 12:21:54 -0700

    everything works in this example

---
+++ integration-test/src/index.ts
@@ -0,0 +1,7 @@
+import { Doctor } from "@lerna-test-typescript-npm/doctor";
+import {Scheduler} from "@lerna-test-typescript-npm/scheduler";
+import { Patient } from "@lerna-test-typescript-npm/patient";
+
+console.log(new Doctor());
+console.log(new Scheduler());
+console.log(new Patient());
